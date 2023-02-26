ce3dff455366455da6cc5c95316d3414921cf14b
Israel <israel_alves77@hotmail.com>
2023-02-26 15:42:03 -0300

    Adicionando a classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,10 +1,13 @@
 package br.com.dio;
 
 import br.com.dio.Model.Gato;
+import br.com.dio.Model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        Livro livro1 = new Livro("Matrix",1000);
+        System.out.println(livro1);
         System.out.println(gato);
 
     }
